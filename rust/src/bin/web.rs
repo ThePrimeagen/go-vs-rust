fede915d1032741649cb70379b8412249c552835
mpaulson <mpaulson@netflix.com>
2022-04-05 21:08:11 -0600

    feat: rust web investigation

---
+++ rust/src/bin/web.rs
@@ -0,0 +1,27 @@
+use actix_web::{get, web, App, HttpServer};
+
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {app_name}!") // <- response with app_name
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix-web"),
+            }))
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
