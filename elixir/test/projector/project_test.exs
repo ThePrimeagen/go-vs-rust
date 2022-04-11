02c01f19327889a1ee41a96d331e0689028eedae
Ryan Winchester <ryan@sevenshores.co>
2022-04-10 23:31:54 -0300

    Docs and doctests for project file

---
+++ elixir/test/projector/project_test.exs
@@ -0,0 +1,7 @@
+defmodule Projector.ProjectTest do
+  use ExUnit.Case, async: true
+
+  alias Projector.Project
+
+  doctest Project, import: true
+end
--- elixir/test/projector_test.exs
+++ elixir/test/projector_test.exs
@@ -2,8 +2,4 @@ defmodule ProjectorTest do
   use ExUnit.Case
 
   doctest Projector, import: true
-
-  test "true is true" do
-    assert true
-  end
 end
